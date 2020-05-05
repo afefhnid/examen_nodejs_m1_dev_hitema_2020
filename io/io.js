dff4081fae41abc60f1bab7f8ba12a4f372a3f55
afefhnid <afefhnid@gmail.com>
2020-05-05 17:37:05 +0200

    io

--- io/io.js
+++ io/io.js
@@ -1,15 +1,9 @@
 const fs = require("fs").promises;
 module.exports.decodeHexFileContent = (filePath) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, "utf8", function (err, data) {
-      if (err) {
-        reject(err);
-      }
-      const buf = Buffer.from(data, "hex");
-      var res = buf.toString("utf8");
-
-      console.log();
-
+    fs.readFile(filePath, (err, contents) => {
+      if (err) reject();
+      const res = Buffer.from(contents.toString(), "hex").toString();
       resolve(res);
     });
   });
