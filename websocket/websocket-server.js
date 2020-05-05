8738f5242ea0baf2c5400d3639540888dd4df1d2
afefhnid <afefhnid@gmail.com>
2020-05-05 17:17:20 +0200

    websoket

--- websocket/websocket-server.js
+++ websocket/websocket-server.js
@@ -1,15 +1,16 @@
-const WebSocket = require('ws')
+const WebSocket = require("ws");
 const webSocketServer = new WebSocket.Server({ port: 3003 });
-  
-webSocketServer.on('connection', webSocket => {
-    webSocket.onmessage = messageEvent => {
-        const message = messageEvent.data;
-        webSocketServer.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    };
+const history = [];
+webSocketServer.on("connection", (webSocket) => {
+  webSocket.send(JSON.stringify(history));
+  webSocket.onmessage = (messageEvent) => {
+    const message = messageEvent.data;
+    webSocketServer.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  };
 });
 
 module.exports = webSocketServer;
