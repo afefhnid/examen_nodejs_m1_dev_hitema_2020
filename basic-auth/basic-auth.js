b7a42fd1fb2e75c08dac67783e1186090d08ec4f
afefhnid <afefhnid@gmail.com>
2020-05-05 17:47:26 +0200

    auth

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -1,26 +1,22 @@
 const crypto = require("crypto");
-
+const HttpStatus = require("http-status-codes");
 function sha1Encode(data) {
   // To be implemented!
-  const hash = crypto.createHash("sha256");
-  const authorization = data;
-  const encoded = authorization.replace("Basic ", "");
-  const decoded = Buffer.from(encoded, "base64").toString("utf8");
-  // 'user:paswword'
-  const authentication = decoded.split(":");
-  return authentication;
+  return crypto.createHash("sha1").update(data).digest("hex");
 }
 
 module.exports.digestAuth = (request, response, next) => {
   // To be implemented!
   const authorization = request.headers.authorization;
-  const authentication = sha1Encode(authorization);
-
+  const encoded = authorization.replace("Basic ", "");
+  const decoded = Buffer.from(encoded, "base64").toString("utf8");
+  const authentication = decoded.split(":");
   const isValid =
-    authentication[0] === "node" && authentication[1] === "password";
+    authentication[0] === "node" &&
+    authentication[1] === sha1Encode("password");
 
   // si pas authentifiÃ©
-  isValid ? next() : response.sendStatus(401);
+  isValid ? next() : response.sendStatus(HttpStatus.UNAUTHORIZED);
 };
 
 //var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
